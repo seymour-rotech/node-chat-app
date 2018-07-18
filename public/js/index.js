69b0296a8b7e38d042e6276eda4e57702ea8c5de
Seymour <seymour@rotech.nz>
2018-07-19 09:04:09 +1200

    JQuery

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server')
 
-    socket.emit('createEmail', {
-        to: 'jen@example.com',
-        text: 'Hey. This is Seymour from index.js.'
-    })
+    // socket.emit('createEmail', {
+    //     to: 'jen@example.com',
+    //     text: 'Hey. This is Seymour from index.js.'
+    // })
 
     // socket.emit('createMessage', {
     //     from: 'Andrew',
@@ -19,9 +19,29 @@ socket.on('disconnect', function () {
 })
 
 socket.on('newMessage', function (message) {
-    console.log('New Message', message)
+    //console.log('New Message', message)
+    var li = jQuery('<li></li>')
+    li.text(`${message.from}: ${message.text}.`)
+
+    jQuery('#messages').append(li)
 })
 
 socket.on('newEmail', function (email) {
     console.log('New Email', email)
+})
+
+// socket.emit('createMessage', {
+//     from : 'Frank',
+//     text : 'Hi'
+// }, function (data) {
+//     console.log('Got it ', data)
+// })
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault()
+    socket.emit('createMessage', {
+        from : 'User',
+        text : jQuery('[name=message]').val()
+    }, function() {        
+    })
 })
--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
 const path = require('path')
-//const http = require('http')
+const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+var {generateMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 
-var {generateMessage} = require('./utils/message')
 var app = express()
-
-//var server = http.createServer(app)
-var server = app.listen(port, () => {
-    console.log(`Starting on Port ${port}`)
-})
+var server = http.createServer(app)
 var io = socketIO.listen(server)
 
 app.use(express.static(publicPath))
 
-io.on('connect', function (socket) {
+server.listen(port, () => {
+   console.log(`Starting on Port ${port}`)
+})
+
+// var server = app.listen(port, () => {
+//     console.log(`Starting on Port ${port}`)
+// })
+
+io.on('connect', (socket) => {
 
     console.log('New User Connected')
 
@@ -25,33 +29,28 @@ io.on('connect', function (socket) {
         console.log('Discounted from Client')
     })
 
-    socket.emit('newMessage', 
-                generateMessage('Admin', 'Welcome to the chat app'))
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
     // {
     //     from : 'Admin',
     //     text : 'Welcome to the chat app',
     //     createAt : new Date().getTime() 
     // })
 
-    socket.broadcast.emit('newMessage', 
-                generateMessage('Admin', 'New User Joined'))
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'))
     // {
     //     from : 'Admin',
     //     text : 'New user joined',
     //     createAt: new Date().getTime()
     // })
     
-    socket.emit('newEmail', 
-                generateMessage('mike@example.com' ,
-                                'Hey. What is going on with server.js'))
-    
+    //socket.emit('newEmail', generateMessage('mike@example.com' , 'Hey. What is going on with server.js'))
     // {
     //     from : 'mike@example.com',
     //     text : 'Hey. What is going on within server.js',
     //     createAt: 123
     // })
 
-    socket.on('createMessage', function (message) {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message)
         // io.emit('newMessage', {
         //     from : message.from,
@@ -59,23 +58,20 @@ io.on('connect', function (socket) {
         //     craetedAt: new Date().getTime()
         // })
 
-           socket.broadcast.emit('newMessage', 
-                            generateMessage(message.from, message.text))
+        io.emit('newMessage', generateMessage(message.from, message.text))
+        callback('This is from server');
+
+        //socket.broadcast.emit('newMessage', generateMessage(message.from, message.text))
         //    {
         //     from : message.from,
         //     text : message.text,
         //     craetedAt: new Date().getTime()
         //    })
-
     })
 
     socket.on('createEmail', (newEmail) => {
         console.log('createEmail', newEmail)
     })
 })
-
-//server.listen(port, () => {
-//    console.log(`Starting on Port ${port}`)
-//})
 
 module.exports = {app}
